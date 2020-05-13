9a4720926438bb5ef0cf5b202c3f4c37d726304d
tuan0105 <tuan0105>
2020-05-13 17:29:43 +0700

    update database

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -1,14 +1,14 @@
-use springmvcbasicfree;
+use quanlynhatro;
 
-insert into role(code,name) values('ADMIN','Quản trị');
+insert into role(code,name) values('ADMIN','Quản trị');
 insert into role(code,name) values('USER','Người dùng');
 
 insert into user(username,password,fullname,status)
-values('admin','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','trương tùng lâm',1);
+values('admin','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','le tien cao',1);
 insert into user(username,password,fullname,status)
-values('nguyenvana','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyễn văn A',1);
+values('nguyenvana','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyen van a',1);
 insert into user(username,password,fullname,status)
-values('nguyenvanb','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyễn văn B',1);
+values('nguyenvanb','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyen van b',1);
 
 INSERT INTO user_role(userid,roleid) VALUES (1,1);
 INSERT INTO user_role(userid,roleid) VALUES (2,2);
