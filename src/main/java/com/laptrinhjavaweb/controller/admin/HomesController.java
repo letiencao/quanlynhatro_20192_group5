e4afdbeffc6b57b2e5a8542de46760159327c89f
letiencao <caole28072000@gmail.com>
2020-05-15 00:42:15 +0700

    add HomesController

---
+++ src/main/java/com/laptrinhjavaweb/controller/admin/HomesController.java
@@ -0,0 +1,69 @@
+package com.laptrinhjavaweb.controller.admin;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.laptrinhjavaweb.dto.HomeDTO;
+import com.laptrinhjavaweb.service.ICategoryService;
+import com.laptrinhjavaweb.service.IHomeService;
+import com.laptrinhjavaweb.util.MessageUtil;
+
+@Controller(value = "homesControllerOfAdmin")
+public class HomesController {
+	@Autowired
+	private IHomeService homeService;
+	
+	@Autowired
+	private ICategoryService categoryService;
+	
+	@Autowired
+	private MessageUtil messageUtil;
+
+	@RequestMapping(value = "/quan-tri/nha-tro/danh-sach", method = RequestMethod.GET)
+	public ModelAndView showList(@RequestParam("page") int page, 
+								 @RequestParam("limit") int limit, HttpServletRequest request) {
+		HomeDTO model = new HomeDTO();
+		model.setPage(page);
+		model.setLimit(limit);
+		ModelAndView mav = new ModelAndView("admin/home/listHome");
+		Pageable pageable = new PageRequest(page - 1, limit);
+		model.setListResult(homeService.findAll(pageable));
+		model.setTotalItem(homeService.getTotalItem());
+		model.setTotalPage((int) Math.ceil((double) model.getTotalItem() / model.getLimit()));
+		if (request.getParameter("message") != null) {
+			Map<String, String> message = messageUtil.getMessage(request.getParameter("message"));
+			mav.addObject("message", message.get("message"));
+			mav.addObject("alert", message.get("alert"));
+		}
+		mav.addObject("model", model);
+		return mav;
+	}
+	
+	@RequestMapping(value = "/quan-tri/nha-tro/chinh-sua", method = RequestMethod.GET)
+	public ModelAndView editNew(@RequestParam(value = "id", required = false) Long id, HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("admin/home/editHome");
+		HomeDTO model = new HomeDTO();
+		if (id != null) {
+			model = homeService.findById(id);
+		}
+		if (request.getParameter("message") != null) {
+			Map<String, String> message = messageUtil.getMessage(request.getParameter("message"));
+			mav.addObject("message", message.get("message"));
+			mav.addObject("alert", message.get("alert"));
+		}
+		mav.addObject("categories", categoryService.findAll());
+		mav.addObject("model", model);
+		return mav;
+	}
+
+}
