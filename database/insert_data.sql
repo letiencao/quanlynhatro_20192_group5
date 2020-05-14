87bc5690201f2d15346fa275038a76797d2e5ac8
Vũ Trọng Nghĩa <vtnghia110@gmail.com>
2020-05-14 12:21:24 +0700

    update database

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -1,14 +1,14 @@
 use quanlynhatro;
 
-insert into role(code,name) values('ADMIN','Quản trị');
-insert into role(code,name) values('USER','Người dùng');
+insert into role(code,name) values('ADMIN','Quáº£n trá»‹');
+insert into role(code,name) values('USER','NgÆ°á»�i dÃ¹ng');
 
 insert into user(username,password,fullname,status)
 values('admin','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','le tien cao',1);
 insert into user(username,password,fullname,status)
 values('nguyenvana','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyen quang tuan',1);
 insert into user(username,password,fullname,status)
-values('nguyenvanb','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','vu duc anh',1);
+values('nguyenvanb','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','vu trong nghia',1);
 
 INSERT INTO user_role(userid,roleid) VALUES (1,1);
 INSERT INTO user_role(userid,roleid) VALUES (2,2);
