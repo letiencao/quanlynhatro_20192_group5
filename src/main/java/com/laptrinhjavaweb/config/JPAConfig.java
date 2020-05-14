9a14f9521ab2a868caeb87e45877a92daf0a366e
letiencao <caole28072000@gmail.com>
2020-05-15 00:39:16 +0700

    update JPAConfig

--- src/main/java/com/laptrinhjavaweb/config/JPAConfig.java
+++ src/main/java/com/laptrinhjavaweb/config/JPAConfig.java
@@ -50,15 +50,15 @@ public class JPAConfig {
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/quanlynhatro");
 		dataSource.setUsername("root");
-		dataSource.setPassword("tuan0105");
+		dataSource.setPassword("123456789");
 		return dataSource;
 	}
 	
 	Properties additionalProperties() {
 		Properties properties = new Properties();
 		//properties.setProperty("hibernate.hbm2ddl.auto", "update");
-		//properties.setProperty("hibernate.hbm2ddl.auto", "create");
-		properties.setProperty("hibernate.hbm2ddl.auto", "none");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		//properties.setProperty("hibernate.hbm2ddl.auto", "none");
 		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
 		return properties;
 	}
