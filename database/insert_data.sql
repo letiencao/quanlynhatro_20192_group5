10d14884655ec3123b4b129068c100eb3e2462bc
unknown <caole28072000@gmail.com>
2020-05-16 19:47:28 +0700

    update insert data

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -15,3 +15,4 @@ values('nguyenvanc','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCY
 INSERT INTO user_role(userid,roleid) VALUES (1,1);
 INSERT INTO user_role(userid,roleid) VALUES (2,2);
 INSERT INTO user_role(userid,roleid) VALUES (3,2);
+INSERT INTO user_role(userid,roleid) VALUES (4,2);
