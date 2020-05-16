e52799c9d390fa98ea375b1b18cb3bc18a026bdf
unknown <anh.vdc@outlook.com>
2020-05-16 19:43:20 +0700

    update NewEntity

--- src/main/java/com/laptrinhjavaweb/entity/NewEntity.java
+++ src/main/java/com/laptrinhjavaweb/entity/NewEntity.java
@@ -10,37 +10,26 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "new")
 public class NewEntity extends BaseEntity {
-	
+
 	@Column(name = "title")
 	private String title;
-	
-	@Column(name = "thumbnail")
-	private String thumbnail;
-	
+
 	@Column(name = "shortdescription", columnDefinition = "TEXT")
 	private String shortDescription;
-	
+
 	@Column(name = "content", columnDefinition = "TEXT")
 	private String content;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private CategoryEntity category;
-	
+	@JoinColumn(name = "category_id")
+	private CategoryEntity category;
+
 	public String getTitle() {
 		return title;
 	}
 
 	public void setTitle(String title) {
 		this.title = title;
-	}
-
-	public String getThumbnail() {
-		return thumbnail;
-	}
-
-	public void setThumbnail(String thumbnail) {
-		this.thumbnail = thumbnail;
 	}
 
 	public String getShortDescription() {
