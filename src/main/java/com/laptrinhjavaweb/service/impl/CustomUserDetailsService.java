65b34f6c976af759341db8b4038caaaec4bc5dde
unknown <anh.vdc@outlook.com>
2020-05-18 09:30:43 +0700

    update user

--- src/main/java/com/laptrinhjavaweb/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/laptrinhjavaweb/service/impl/CustomUserDetailsService.java
@@ -37,6 +37,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 		MyUser myUser = new MyUser(userEntity.getUserName(), userEntity.getPassword(), 
 							true, true, true, true, authorities);
 		myUser.setFullName(userEntity.getFullName());
+		myUser.setPhone(userEntity.getPhone());
 		return myUser;
 	}
 
