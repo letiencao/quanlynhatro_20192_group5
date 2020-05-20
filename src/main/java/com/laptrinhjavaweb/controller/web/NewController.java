ff9a6a9452196220d271a35cb07e05e36ff96a6c
unknown <vtnghia110@gmail.com>
2020-05-20 15:04:25 +0700

    update NewCOntroller

---
+++ src/main/java/com/laptrinhjavaweb/controller/web/NewController.java
@@ -0,0 +1,51 @@
+package com.laptrinhjavaweb.controller.web;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.laptrinhjavaweb.dto.NewDTO;
+import com.laptrinhjavaweb.service.ICategoryService;
+import com.laptrinhjavaweb.service.INewService;
+import com.laptrinhjavaweb.util.MessageUtil;
+
+@Controller(value = "newControllerOfWeb")
+public class NewController {
+	@Autowired
+	private INewService newService;
+	
+	@Autowired
+	private ICategoryService categoryService;
+	
+	@Autowired
+	private MessageUtil messageUtil;
+
+	
+	
+	@RequestMapping(value = "/trang-chu/bai-viet/chinh-sua", method = RequestMethod.GET)
+	public ModelAndView editNew(@RequestParam(value = "id", required = false) Long id, HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("web/new/edit");
+		NewDTO model = new NewDTO();
+		if (id != null) {
+			model = newService.findById(id);
+		}
+		if (request.getParameter("message") != null) {
+			Map<String, String> message = messageUtil.getMessage(request.getParameter("message"));
+			mav.addObject("message", message.get("message"));
+			mav.addObject("alert", message.get("alert"));
+		}
+		mav.addObject("categories", categoryService.findAll());
+		mav.addObject("model", model);
+		return mav;
+	}
+
+}
