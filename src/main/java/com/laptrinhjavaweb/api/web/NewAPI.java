bc113603b8b35cddc66d8902c4cfc4fa1b83cd30
unknown <vtnghia110@gmail.com>
2020-05-20 14:57:11 +0700

    update NewAPI.java

--- src/main/java/com/laptrinhjavaweb/api/web/NewAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/web/NewAPI.java
@@ -1,9 +1,33 @@
 package com.laptrinhjavaweb.api.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.laptrinhjavaweb.dto.NewDTO;
+import com.laptrinhjavaweb.service.INewService;
 
 @RestController(value = "newAPIOfWeb")
 public class NewAPI {
 	
+	@Autowired
+	private INewService newService;
 	
+	@PostMapping("/api/web/new")
+	public NewDTO createNew(@RequestBody NewDTO newDTO) {
+		return newService.save(newDTO);
+	}
+	
+	@PutMapping("/api/web/new")
+	public NewDTO updateNew(@RequestBody NewDTO updateNew) {
+		return newService.save(updateNew);
+	}
+	
+	@DeleteMapping("/api/web/new")
+	public void deleteNew(@RequestBody long[] ids) {
+		newService.delete(ids);
+	}
 }
