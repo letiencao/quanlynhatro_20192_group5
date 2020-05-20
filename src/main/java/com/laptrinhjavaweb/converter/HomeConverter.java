b697681a32dca268eba0122bdfe42e397269a3ba
unknown <vtnghia110@gmail.com>
2020-05-20 15:06:08 +0700

    update HomeConverter

--- src/main/java/com/laptrinhjavaweb/converter/HomeConverter.java
+++ src/main/java/com/laptrinhjavaweb/converter/HomeConverter.java
@@ -1,4 +1,4 @@
-package com.laptrinhjavaweb.converter;
+ package com.laptrinhjavaweb.converter;
 
 import org.springframework.stereotype.Component;
 
@@ -8,12 +8,14 @@ import com.laptrinhjavaweb.entity.HomeEntity;
 public class HomeConverter {
 	public HomeDTO toDto(HomeEntity entity) {
 		HomeDTO result = new HomeDTO();
+		result.setId(entity.getId());
 		result.setThumbnail(entity.getThumbnail());
 		result.setWard(entity.getWard());
 		result.setDistrict(entity.getDistrict());
 		result.setCity(entity.getCity());
 		result.setArea(entity.getArea());
 		result.setRentCost(entity.getRentCost());
+		result.setCategoryCode(entity.getCategory().getCode());
 		return result;
 	}
 	
@@ -25,6 +27,7 @@ public class HomeConverter {
 		result.setCity(dto.getCity());
 		result.setArea(dto.getArea());
 		result.setRentCost(dto.getRentCost());
+		
 		return result;
 	}
 	public HomeEntity toEntity(HomeEntity result, HomeDTO dto) {
